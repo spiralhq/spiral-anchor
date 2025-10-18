809af1090fbf4c831382f0f9f7b5f40166c52ff6
Ricardo Costa <ricardocosta1601@gmail.com>
2025-10-18 19:37:06 -0300

    feat: first commit with counter template

---
+++ programs/spiral/src/lib.rs
@@ -0,0 +1,57 @@
+use anchor_lang::prelude::*;
+
+declare_id!("ATcPjmLfdDKyjAiyfrPJTCp6CXrh5gGNnjn2TviMfo6L");
+
+#[program]
+pub mod counter {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.bump = ctx.bumps.counter;
+        msg!("Counter account created! Current count: {}", counter.count);
+        msg!("Counter bump: {}", counter.bump);
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous counter: {}", counter.count);
+        counter.count = counter.count.checked_add(1).unwrap();
+        msg!("Counter incremented! Current count: {}", counter.count);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        seeds = [b"counter"],
+        bump, 
+        payer = user,
+        space = 8 + Counter::INIT_SPACE
+    )]
+    pub counter: Account<'info, Counter>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(
+        mut,
+        seeds = [b"counter"],
+        bump = counter.bump,
+    )]
+    pub counter: Account<'info, Counter>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Counter {
+    pub count: u64,
+    pub bump: u8,
+}
